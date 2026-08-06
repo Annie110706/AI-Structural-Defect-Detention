4523e2627043460824c54a1d9fdc38a8a8eb9608
Annie110706 <anvitnandkeolyar@gmail.com>
2026-08-06 12:53:14 +0530

    Final project update

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import tensorflow as tf
+import pandas as pd
+import time
+import csv 
+from datetime import datetime
+
+from pdf_report import generate_report
 
 
 # Load the trained AI model
@@ -19,10 +25,34 @@ st.set_page_config(
     page_icon="🏗️",
     layout="centered"
 )
+# ==========================
+# Sidebar
+# ==========================
 
+st.sidebar.title("📋 Project Info")
+
+st.sidebar.write("""
+### AI Structural Defect Detection
+
+This application uses a Deep Learning model to detect structural cracks from images.
+
+**Model:** MobileNetV2
+
+**Classes**
+- Crack
+- No Defect
+
+**Input Size**
+224 × 224 pixels
+""")
+
+st.sidebar.divider()
+
+st.sidebar.success("Developed using Python, TensorFlow & Streamlit")
 # Title
 
 st.title("🏗️ Structural Defect Detection using AI")
+st.caption("AI-powered concrete crack detection using Deep Learning")
 
 st.write(
     "Upload an image of a concrete or masonry surface and "
@@ -30,6 +60,7 @@ st.write(
 )
 
 st.divider()
+st.info("📌 Supported file types: JPG, JPEG, PNG")
 
 # Upload Image
 
@@ -45,13 +76,21 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
     # Display uploaded image
-    st.image(
+    # Create two columns
+    col1, col2 = st.columns([1, 1])
+
+# Left Column
+    with col1:
+        st.image(
         image,
         caption="Uploaded Image",
         use_container_width=True
     )
 
-    st.write("")
+# Right Column
+    with col2:
+        st.subheader("Prediction Result")
+        st.write("")
 
     # Detect Button
     if st.button("🔍 Detect Defect"):
@@ -87,6 +126,19 @@ if uploaded_file is not None:
 
             confidence = np.max(prediction)
 
+            # Save Prediction history
+            current_time = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+            with open("prediction_history.csv","a",
+                      newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow([
+                    current_time,
+                    predicted_class,
+                    f"{confidence:.2%}"
+                ])
+            
+
         st.divider()
 
         st.subheader("Prediction Result")
@@ -104,8 +156,40 @@ if uploaded_file is not None:
             label="Confidence",
             value=f"{confidence:.2%}"
         )
+        st.progress(float(confidence))
 
-        # Raw prediction (optional)
-        with st.expander("Prediction Details"):
-            st.write("Prediction Vector:", prediction)
-            st.write("Predicted Class:", predicted_class)
+        #Generate PDF report
+        pdf_file = generate_report(predicted_class,confidence)
+
+        with open(pdf_file, "rb")as file:
+            st.download_button(
+                label="📥 Download Inspection Report",
+                data=file,
+                file_name="Structural_Defect_Report.pdf",
+                mime="application/pdf"
+            )
+        with st.expander("📊 Prediction Details"):
+
+            st.write("### Class Probabilities")
+
+        for i, class_name in enumerate(class_names):
+            st.write(f"{class_name}: {prediction[0][i]:.2%}")
+
+    # Create DataFrame
+        df = pd.DataFrame({
+        "Class": class_names,
+        "Probability": prediction[0]
+        })
+
+        st.bar_chart(df.set_index("Class"))
+
+        st.write("---")
+        st.write("Predicted Class:", predicted_class)
+        st.write("---")
+        st.subheader("Prediction History")
+        history = pd.read_csv("prediction_history.csv")
+        st.dataframe(history,use_container_width=True)
+        if st.button("Clear History"):
+            history.iloc[0:0].to_csv("predicition_history.csv", index=False)
+            st.success("Prediction history cleared successfully!")
+            st.rerun()
